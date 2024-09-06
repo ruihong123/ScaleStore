edf327eab23afee2c376da8c5b9896dc32a423de
ruihong123 <wang4996@purdue.edu>
2024-09-06 16:34:54 -0400

    bash scripts for test

--- scripts/sync_all.sh
+++ scripts/sync_all.sh
@@ -100,7 +100,7 @@ function run_bench() {
 #    ssh -o StrictHostKeyChecking=no $node "sudo /etc/init.d/openibd restart"
 #    ssh -o StrictHostKeyChecking=no $node "sudo mst start"
 #    ssh -o StrictHostKeyChecking=no $node "echo '/proj/purduedb-PG0/logs/core$node' | sudo tee /proc/sys/kernel/core_pattern"
-    ssh ${ssh_opts} $node "ifconfig ib0 192.168.100.$i"
+    ssh ${ssh_opts} $node "sudo ifconfig ib0 192.168.100.$i"
     i=$((i+1))
 
   done
@@ -119,7 +119,7 @@ function run_bench() {
 #    ssh ${ssh_opts} $node "sudo mkdir /mnt/core_dump && sudo mkfs.ext4 /dev/sda4 && sudo mount /dev/sda4 /mnt/core_dump"
 
     ssh ${ssh_opts} $node "echo '$core_dump_dir/core$compute' | sudo tee /proc/sys/kernel/core_pattern" &
-    ssh ${ssh_opts} $node "ifconfig ib0 192.168.100.$i"
+    ssh ${ssh_opts} $node "sudo ifconfig ib0 192.168.100.$i"
     i=$((i+1))
 #    ssh -o StrictHostKeyChecking=no $node  "sudo mount /dev/sda4 /mnt/core_dump" &
 
