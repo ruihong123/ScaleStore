ced2640dd9a1cc2bae812c8a7dc2e0cf32bd8903
ruihong123 <wang4996@purdue.edu>
2024-09-08 17:24:49 -0400

    bash scripts for test

--- scripts/install_dep.sh
+++ scripts/install_dep.sh
@@ -68,33 +68,24 @@ function run_bench() {
   while [ $n -lt $nshard ]
   do
     echo "Set up the ${compute_shard[$n]}"
-#    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt install -y gcc-10 g++-10" & # python3.6
-#    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "git clone https://github.com/google/cityhash && cd cityhash/ && ./configure && make all check CXXFLAGS='-g -O3' && sudo make install && cd .. && sudo apt-get update && sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev memcached libboost-all-dev libgflags-dev libaio-dev" & # python3.6
-#    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo mkdir /mnt/core_dump && sudo mkfs.ext4 /dev/sda4 && sudo mount /dev/sda4 /mnt/core_dump" &
-    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo apt-get install -y libaio-dev" &
+    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt install -y gcc-10 g++-10" & # python3.6
+    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "git clone https://github.com/google/cityhash && cd cityhash/ && ./configure && make all check CXXFLAGS='-g -O3' && sudo make install && cd .. && sudo apt-get update && sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev memcached libboost-all-dev libgflags-dev libaio-dev" & # python3.6
+    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo mkdir /mnt/core_dump && sudo mkfs.ext4 /dev/sda4 && sudo mount /dev/sda4 /mnt/core_dump" &
+#    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo apt-get install -y libaio-dev" &
 
 #    ssh -o StrictHostKeyChecking=no ${compute_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git checkout $gitbranch && git pull &&  cmake -DCMAKE_BUILD_TYPE=Release .. && make db_bench Server -j 32 > /dev/null && sudo apt install numactl -y " &
 #    screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd MemoryEngine/ && mkdir release &&  cd release && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt install numactl -y &&screen -d -m pwd && cd /users/Ruihong && git clone --recurse-submodules $github_repo && cd MemoryEngine/ && mkdir release &&  cd release && cmake -DCMAKE_BUILD_TYPE=Release .. && sudo apt install numactl -y &&
     echo "Set up the ${memory_shard[$n]}"
-#    ssh -o StrictHostKeyChecking=no ${memory_shard[$n]}  "sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt install -y gcc-10 g++-10" & # python3.6
-#    ssh -o StrictHostKeyChecking=no ${memory_shard[$n]}  "git clone https://github.com/google/cityhash && cd cityhash/ && ./configure && make all check CXXFLAGS='-g -O3' && sudo make install && cd .. && sudo apt-get update && sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev memcached libboost-all-dev libgflags-dev libaio-dev" & # python3.6
-#    ssh -o StrictHostKeyChecking=no ${memory_shard[$n]}  "sudo mkdir /mnt/core_dump && sudo mkfs.ext4 /dev/sda4 && sudo mount /dev/sda4 /mnt/core_dump" &
-    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo apt-get install -y libaio-dev" &
+    ssh -o StrictHostKeyChecking=no ${memory_shard[$n]}  "sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt install -y gcc-10 g++-10" & # python3.6
+    ssh -o StrictHostKeyChecking=no ${memory_shard[$n]}  "git clone https://github.com/google/cityhash && cd cityhash/ && ./configure && make all check CXXFLAGS='-g -O3' && sudo make install && cd .. && sudo apt-get update && sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev memcached libboost-all-dev libgflags-dev libaio-dev" & # python3.6
+    ssh -o StrictHostKeyChecking=no ${memory_shard[$n]}  "sudo mkdir /mnt/core_dump && sudo mkfs.ext4 /dev/sda4 && sudo mount /dev/sda4 /mnt/core_dump" &
+#    ssh -o StrictHostKeyChecking=no ${compute_shard[$n]}  "sudo apt-get install -y libaio-dev" &
 
 #   git clone https://github.com/google/cityhash &&
 #    ssh -o StrictHostKeyChecking=no ${memory_shard[n]} "screen -d -m pwd && cd /users/Ruihong/TimberSaw/build && git checkout $gitbranch && git pull &&  cmake -DCMAKE_BUILD_TYPE=Release .. && make db_bench Server -j 32 > /dev/null && sudo apt install numactl -y" &
     n=$((n+1))
     sleep 1
   done
-  for node in ${memory_shard[@]}
-  do
-    echo "Rsync the $node"
-    rsync -a ~/MemoryEngine $node:/users/Ruihong/
-  done
-  for node in ${compute_shard[@]}
-  do
-    echo "Rsync the $node"
-    rsync -a ~/MemoryEngine $node:/users/Ruihong/
-  done
+
 	}
 	run_bench
