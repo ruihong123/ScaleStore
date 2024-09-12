f388069bb802f005adac5d7504e753bbf7f61ff7
ruihong123 <wang4996@purdue.edu>
2024-09-12 13:45:01 -0400

    bash scripts for test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -503,12 +503,12 @@ run_node_test() {
 # node test
 echo "**************************run node test****************************"
 result_file=$bin/results/node
-node_range="1 2 4 8"
-thread_range="16"
+node_range="8"
+thread_range="1 2 4 8 16"
 remote_range="100"
-shared_range="0 30 60 100"
+shared_range="100"
 size_grow=0 # 0 not grow, 1 grow with node number
-read_range="0 50 95 100"
+read_range="100"
 space_range="0"
 time_range="0"
 workload_range="0" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
