09813412a4aeeecce5fce5910d6f81d074008d82
ruihong123 <wang4996@purdue.edu>
2024-09-26 16:11:51 -0400

    bash scripts for test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -149,7 +149,7 @@ run() {
           script_memory="cd ${bin_dir} && $numacommand ./MemoryServer -worker=$thread -dramGB=$dramGBMemory -nodes=$numberNodes -messageHandlerThreads=$messagehdt   -ownIp=$ibip -pageProviderThreads=$pp -coolingPercentage=10 -freePercentage=$fp -csvFile=ycsb_data_scalability_new_hashtable.csv -tag=NO_DELEGATE -evictCoolestEpochs=0.5 --ssd_path=$ssdPath --ssd_gib=$ssdGBMemory -prob_SSD=$probSSD > $log_file.$ip 2>&1"
           echo "start worker: ssh ${ssh_opts} ${memory} '$script_memory' &"
           ssh ${ssh_opts} ${memory} " sudo touch /mnt/core_dump/data.blk; sudo chown -R Ruihong:purduedb-PG0 /mnt/core_dump; echo '$core_dump_dir/core$memory' | sudo tee /proc/sys/kernel/core_pattern"
-          ssh ${ssh_opts} ${memory} "ulimit -S -c unlimited &&  $script_memory" &
+          ssh ${ssh_opts} ${memory} "ulimit -S -c unlimited &&  $script_memory | tee -a $log_file.$ip" &
           i=$((i+1))
 #        	if [ "$i" = "$node" ]; then
 #        		break
