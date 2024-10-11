a823323e96e23f158d70fbdd2e59e9412af51cdb
ruihong123 <wang4996@purdue.edu>
2024-10-10 22:21:20 -0400

    bash scripts for test

--- frontend/Microbench/microbench.cpp
+++ frontend/Microbench/microbench.cpp
@@ -126,7 +126,34 @@ __inline__ unsigned long long rdtsc(void) {
     return ((unsigned long long)lo) | (((unsigned long long)hi) << 32);
 }
 
+// A good 64-bit random number generator based on std::mt19937_64
+class Random64 {
+private:
+    std::mt19937_64 generator_;
 
+public:
+    explicit Random64(uint64_t s) : generator_(s) { }
+
+    // Generates the next random number
+    uint64_t Next() { return generator_(); }
+
+    // Returns a uniformly distributed value in the range [0..n-1]
+    // REQUIRES: n > 0
+    uint64_t Uniform(uint64_t n) {
+        return std::uniform_int_distribution<uint64_t>(0, n - 1)(generator_);
+    }
+
+    // Randomly returns true ~"1/n" of the time, and false otherwise.
+    // REQUIRES: n > 0
+    bool OneIn(uint64_t n) { return Uniform(n) == 0; }
+
+    // Skewed: pick "base" uniformly from range [0,max_log] and then
+    // return "base" random bits.  The effect is to pick a number in the
+    // range [0,2^max_log-1] with exponential bias towards smaller numbers.
+    uint64_t Skewed(int max_log) {
+        return Uniform(uint64_t(1) << Uniform(max_log + 1));
+    }
+};
 //extern uint64_t cache_invalidation[MAX_APP_THREAD];
 //extern uint64_t cache_hit_valid[MAX_APP_THREAD][8];
 #ifdef GETANALYSIS
@@ -371,8 +398,11 @@ void Init(Memcached* memcached, PID data[], PID access[], bool shared[], int id,
         }
 
     }
+    Random64 rand(*seedp);
     //access[0] = data[0];
-    access[0] = data[GetRandom(0, STEPS, seedp)];
+    access[0] = data[rand.Uniform(STEPS)];
+
+//    access[0] = data[GetRandom(0, STEPS, seedp)];
 #ifdef STATS_COLLECTION
     stat_lock.lock();
   gen_accesses.insert(TOBLOCK(access[0]));
@@ -413,7 +443,7 @@ void Init(Memcached* memcached, PID data[], PID access[], bool shared[], int id,
 //            }
         } else {
             if (FLAGS_zip_workload == 0){
-                PID n = data[GetRandom(0, STEPS, seedp)];
+                PID n = data[rand.Uniform(STEPS)];
                 next = n;
 //                while (TOPAGE(n) == TOPAGE(access[i - 1])) {
 //                    n = data[GetRandom(0, STEPS, seedp)];
