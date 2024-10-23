21642399c87840c1ee0a151b1c60852b3f41553f
ruihong123 <wang4996@purdue.edu>
2024-10-23 13:08:42 -0400

    bash scripts for test

--- scripts/bench_micro.sh
+++ scripts/bench_micro.sh
@@ -512,9 +512,9 @@ remote_range="100"
 shared_range="100"
 size_grow=0 # 0 not grow, 1 grow with node number
 read_range="0"
-space_range="50"
+space_range="0"
 time_range="0"
-workload_range="0" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
+workload_range="1" # 0 uniform, 1 single zipfian, n >1 multispot zipfian.
 zipfian_alpha_range="0.99" #make sure workload = 1 if we want to test zipfian.
 #
 op_range="1" # use 1
